5f7278948d7ae484fb40605bc55d65938551b5d7
Julian <home@Julians-MBP.lan>
2021-11-05 14:50:29 -0500

    '2021-Nov-05'

--- create.sh
+++ create.sh
@@ -9,17 +9,18 @@
 # gcloud cli # https://cloud.google.com/sdk/docs/install
 # scaffold # https://skaffold.dev/docs/install/
 
-# ==========================================
-
 export sh_dir=$(dirname "${BASH_SOURCE[0]}") || exit 1
 export parent_path="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")$sh_dir" || exit 1
 export grandparent_path="$(cd "$(dirname "$1")"; pwd -P)/" || exit 1
 export creation_path="$parent_path/creation" || exit 1
-export utils_path="$parent_path/utils" || exit 1
 export development_path="$parent_path/development" || exit 1
+export utils_path="$parent_path/utils" || exit 1
 
-. $utils_path/colors.sh
+. $utils_path/colors.sh  || exit 1
+
 echo -e "${LCYAN}RUNNING CREATE.SH"
+
+echo -e "${LPURPLE}===================================================================================="
 
 echo -e "${GREEN}Welcome to the create script. Let's get started creating your project!"
 echo -n -e "Enter a PROJECT_ID (i.e. '${LCYAN}mydomain-com${GREEN}'):\n
@@ -33,41 +34,52 @@ echo -e "${CYAN}Congrats on starting a new project!\n
 By the way, '${LCYAN}$PROJECT_ID${CYAN}' is an excellent name choice, 
 I wish I had thought of it first!${WHITE}\n"
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
 echo -e "${LCYAN}RUNNING GCLOUD AUTH LOGIN"
 gcloud auth login 
 
+echo -e "${LCYAN}CREATE-PROJECT.SH"
 bash $creation_path/create-project.sh || exit 1
+
+echo -e "${LCYAN}CLONE-DEMO.SH"
 bash $creation_path/clone-demo.sh || exit 1
 
+echo -e "${LCYAN}GET-ACCOUNT-ID.SH"
 . $utils_path/get-account-id.sh
+
+echo -e "${LCYAN}TURN-ON-BILLING.SH"
 bash $creation_path/turn-on-billing.sh || exit 1
 
+echo -e "${LCYAN}ENABLE-PROJECT.SH.SH"
 bash $utils_path/enable-project.sh || exit 1
 
 echo -e "${LRED}Fair Warning: ${CYAN}These next steps may take a few minutes (est. 20 total) ... 
 time to get some ${LPURPLE}coffee${CYAN} or play with the ${LPURPLE}doge${CYAN}."
 
+echo -e "${LCYAN}CREATE-AUTO.SH"
 bash $creation_path/create-auto.sh || exit 1 # Took about 10 minutes for me
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
+echo -e "${LCYAN}CREATING CREDENTIALS"
 export SERVICE_ACCOUNT="$PROJECT_ID-service-account" || exit 1
 export certs_path="$grandparent_path/$PROJECT_ID/certs/" || exit 1
 export cert_file_path="$certs_path/$SERVICE_ACCOUNT.json" || exit 1
 mkdir $certs_path || exit 1
 touch $certs_path/$SERVICE_ACCOUNT.json || exit 1
 
+echo -e "${LCYAN}CREATE-SERVICE-ACCOUNT.SH"
 bash $creation_path/create-service-account.sh || exit 1
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
+echo -e "${LCYAN}APPLY-MANIFESTS.SH"
 bash $creation_path/apply-manifests.sh || exit 1
 
+echo -e "${LCYAN}GET-PODS.SH"
 bash $utils_path/get-pods.sh || exit 1
 
-bash $creation_path/dev-init.sh || exit 1
-
+echo -e "${LCYAN}GET-FRONTEND-IP.SH"
 bash $utils_path/get-frontend-ip.sh || exit 1 # Can take about 5 minutes
 
--- dev.sh
+++ dev.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# ==========================================
-
 export sh_dir=$(dirname "${BASH_SOURCE[0]}")
 export parent_path="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")$sh_dir"
 export grandparent_path="$(cd "$(dirname "$1")"; pwd -P)/" || exit 1
@@ -13,8 +11,9 @@ export utils_path="$parent_path/utils"
 
 echo -e "${LCYAN}RUNNING DEV.SH"
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
+echo -e "${LCYAN}GETTING PROJECT_ID"
 exported_pid=$($PROJECT_ID | wc -c)  || exit 1
 if [ "$exported_pid" -eq "0" ]; then
     echo -n -e "Enter your PROJECT_ID:\n"
@@ -22,18 +21,23 @@ if [ "$exported_pid" -eq "0" ]; then
     export PROJECT_ID
 fi
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
 echo -e "${LCYAN}RUNNING GCLOUD AUTH LOGIN"
 gcloud auth login 
+
+echo -e "${LCYAN}EXPORTING CREDENTIALS"
 export SERVICE_ACCOUNT="$PROJECT_ID-service-account" || exit 1
 export certs_path="$grandparent_path/$PROJECT_ID/certs/" || exit 1
 export cert_file_path="$certs_path/$SERVICE_ACCOUNT.json" || exit 1
 export GOOGLE_APPLICATION_CREDENTIALS=$cert_file_path || exit 1
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
-minikube start
+echo -e "${LCYAN}STARTING MINICUBE"
+minikube start --cpus=8 --memory 16000 --disk-size 64g || exit 1
+
+echo -e "${LPURPLE}===================================================================================="
 
 echo -e "${LCYAN}RUNNING SETUP-DEV.SH"
 bash $development_path/setup-dev.sh || exit 1
@@ -44,14 +48,15 @@ bash $utils_path/enable-project.sh || exit 1
 echo -e "${LCYAN}RUNNING GET-NODES.SH"
 bash $utils_path/get-nodes.sh || exit 1
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
+echo -e "${LCYAN}HEADING INTO PROJECT ROOT"
 cd $PROJECT_ID || exit 1
 
-echo -e "${LCYAN}RUNNING SKAFFOLD-DEV.SH"
-bash $development_path/skaffold-dev.sh || exit 1
+echo -e "${LCYAN}RUNNING SKAFFOLD RUN"
+skaffold run --default-repo=gcr.io/$PROJECT_ID || exit 1
 
-# ==========================================
+echo -e "${LPURPLE}===================================================================================="
 
 echo -e "${LCYAN}RUNNING GET-PODS.SH"
 bash $utils_path/get-pods.sh || exit 1
